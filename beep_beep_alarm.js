0aae9e8129a2d925fb0218f7cebff66ec4d5a876
jmei2 <jmei2@uchicago.edu>
2022-10-15 14:13:42 -0500

    pink screen

---
+++ beep_beep_alarm.js
@@ -0,0 +1,19 @@
+/*
+beep beep alarm by michelle, chelsea, william, and jacqs
+
+amplitudes to test:
+
+*/
+
+let screen_horizontal = 700;
+let screen_vertical = screen_horizontal;
+
+function setup() {
+    createCanvas(screen_horizontal, screen_vertical);
+    background(255, 199, 216);
+    Text("Welcome to our study!")
+}
+
+function draw() {
+    
+}
