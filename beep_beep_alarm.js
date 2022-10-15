9da1031cf5f68aa655546c1e470d11aaa1bbf5c1
Michelle Chen <michellechen@Michelles-Mac.local>
2022-10-15 14:44:51 -0500

    changed src

--- beep_beep_alarm.js
+++ beep_beep_alarm.js
@@ -15,7 +15,6 @@ let screen_vertical = screen_horizontal;
 function setup() {
     createCanvas(screen_horizontal, screen_vertical);
     background(255, 199, 216);
-    Text("Welcome to our study!")
 }
 
 function draw() {
