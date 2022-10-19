08ca203d95887875ece66c976f7863c63e75ec6f
jmei2 <jmei2@uchicago.edu>
2022-10-18 22:35:03 -0500

    added amps and consolidated final print

--- public/beep_beep_alarm.js
+++ public/beep_beep_alarm.js
@@ -10,26 +10,30 @@ frequencies to test:
 */
 // variables
 let frequencies = [40, 300, 500, 800, 1000, 2000, 5000, 8000, 10000, 12000];
+let freqcpy = frequencies.map((x) => x);
+let freqcpy2 = frequencies.map((x) => x);
 let amplitudes = [0.2, 0.6, 1];
 let trial_count = 0;
 let max_trials = 10;
-let part; // corresponding to the 3 amplitudes each frequency is played at, so part 1 2 3
+let part = 0; // corresponding to the 3 amplitudes each frequency is played at, so part 1 2 3
+let max_parts = 3;
 let trial_start = false;
 let screen_horizontal = 700;
 let screen_vertical = screen_horizontal;
+let fin_order = [];
+let fin_index = 0;
 
 let osc, freq, amp;
 
 function setup() {
     let cnv = createCanvas(screen_horizontal, screen_vertical);
-    cnv.mousePressed(playOscillator);
     background(255, 199, 216);
     //initiates the oscillator
     osc = new p5.Oscillator('sine');
 
     //creating the button and telling playOscillator to run after it's clicked
     button = createButton('play');
-    button.position(20, 50);
+    button.position(20, 60);
     button.mousePressed(playOscillator);
 }
 
@@ -43,32 +47,58 @@ function draw() {
       text("Trials: " + trial_count + " out of " + max_trials, 10, 30);
     } else {
       background(255, 199, 216);
+      button.remove();
       text("Congrats! Study completed.", 10, 30);
     }
 }
 
 // this function handles the sound logic and runs when the button is clicked
 function playOscillator() {
-    if (!trial_start) {
-      trial_start = true;
-    }
-    // TODO: implement amplitude logic (increment trials to 30?)
-    //          right now amplitude is fixed at 0.5
-    if(trial_count < max_trials) {
-      background(255, 199, 216);
-      osc.start();
-      // this is the logic chelsea wrote to randomize frequency!
-      // it removes used frequencies from the array after each trial
-      freq = random(frequencies);
-      let freq_index = frequencies.indexOf(freq);
-      frequencies.splice(freq_index, 1);
+  if (!trial_start) {
+    trial_start = true;
+  }
+  
+  if (trial_count <= max_trials && part < max_parts) {
+    background(255, 199, 216);
+    osc.start();
+    // this is the logic chelsea wrote to randomize frequency!
+    // it removes used frequencies from the array after each trial
+    freq = random(frequencies);
+    let freq_index = frequencies.indexOf(freq);
+    frequencies.splice(freq_index, 1);
 
-      osc.amp(0.5);
-      osc.freq(freq);
-      //this tells the oscillator to stop after...0.8 unit of time
-      //lol its not measured in seconds idk what it means
-      osc.stop(0.8);
-      console.log(freq);
-    }
+    amp = amplitudes[part];
+    osc.amp(amp);
+    osc.freq(freq);
+    //this tells the oscillator to stop after...0.8 unit of time
+    //lol its not measured in seconds idk what it means
+    osc.stop(0.8);
+    // replacing the console.log(freq) with an array (amp, freq) that
+    // keeps track and shows at the end
+    fin_order.push([amp, freq]);
     trial_count++;
+    fin_index++;
+  }
+
+  // moving onto next part
+  if (trial_count > max_trials) {
+    textSize(16);
+    text("Moving onto the next part! Press the button again when you're ready.", 20, 90);
+    part++;
+    trial_count = 0;
+    frequencies = freqcpy;
+    if (part == 2)
+      frequencies = freqcpy2;
+  }
+
+  // print everything to console at end
+  if (part == 3) {
+    trial_count = 50; // trial_count > 30 triggers end of study
+    console.log("finished");
+    let str = "";
+    for (var i=0; i<30; i++) {
+      str += '(' + fin_order[i][0] + ", " + fin_order[i][1] + ')';
+    }
+    console.log(str);
+  }
 }
